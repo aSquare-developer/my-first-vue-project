6bd42ee441748a9386abdc483d61909955c9a62d
aSquare <artur.anissimov@gmail.com>
2023-04-06 14:35:56 +0300

    From Mixins to Composables! Code Re-use Mixins Composables!

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,12 @@
+<script setup>
+import { useFlash } from "@/composables/useFlash";
+
+let { flash } = useFlash();
+</script>
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <p>
+      <button @click="flash('Ops!', 'It Works on the About Page!', 'info')">Click me!</button>
+    </p>
   </div>
 </template>
-
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
