1144243c65306fafdbc985385214faf87185b2f7
aSquare <artur.anissimov@gmail.com>
2023-04-08 21:02:14 +0300

    Refactor to defineProps and defineEmits v-model for Custom Components defineProps defineEmits

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,20 +1,16 @@
 <script setup>
-import { useStorage } from "@/composables/useStorage";
 
-let food = useStorage('food');
-let age = useStorage('age')
+import TabbableTextarea from "@/components/TabbableTextarea.vue";
+import {ref} from "vue";
 
+let comment = ref('test value');
 
 </script>
 
 <template>
   <main>
-    <p>
-      What is your favorite food? <input type="text" v-model="food">
-    </p>
-
-    <p>
-      How old are you? <input type="text" v-model="age">
-    </p>
+    <form>
+      <TabbableTextarea v-model="comment" style="width: 100%; height: 300px;"/>
+    </form>
   </main>
 </template>
